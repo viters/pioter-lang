87f171749eb9807437be17b52cbf7ac6ea04e7c3
Łukasz Szcześniak <say.hello@viters.net>
2018-06-17 12:33:07 +0200

    Code filename now will be provided via args

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate pest;
 extern crate pest_derive;
 
 use parser::parse;
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use tokenizer::tokenize;
@@ -12,7 +13,14 @@ mod parser;
 mod stdlib;
 
 fn main() {
-  let contents = read_file("code2.ptr");
+  let args: Vec<String> = env::args().collect();
+
+  if args.get(0).is_none() {
+    eprintln!("Please provide file name as parameter");
+    panic!();
+  }
+
+  let contents = read_file(args.get(1).unwrap());
   let tokens = tokenize(&contents);
   if tokens.is_err() {
     let errors = tokens.err().unwrap();
